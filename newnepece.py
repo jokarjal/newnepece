365d87f46d6cc258cde89dea309e97282ff942fe
jokarjal <joonas.karjalainen@gmail.com>
2017-05-17 21:23:35 +0300

    Comments & coming to senses

--- newnepece.py
+++ newnepece.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+newnepece.py
+Usage: newnepece.py
+Checks MangaStream page for new One Piece chapters
+and should it find a new one, prints the chapter number,
+name and URL to command line.
+"""
+
 import urllib.request
 import os
 import re
@@ -10,8 +18,18 @@ import sys
 URL = "http://mangastream.com/manga/one_piece"
 newest = 0
 save_location = "newnepece.dat"
+chapter = ()
 
 def getfromurl(url):
+    """
+    Gets HTML for the specific page and returns it as array of bytes.
+
+    Args: 
+        url (str): location of the page
+
+    Returns:
+        bytes, the HTML page requested
+    """
     try:
         html = urllib.request.urlopen(url).read()
         return html
@@ -20,8 +38,18 @@ def getfromurl(url):
         return
 
 def parse(data):
+    """
+    Parses a list of elements for chapters, returns either Boolean True
+    if there is a new chapter or Boolean False if there are no new chapters.
+    
+    Args:
+        data (list): A list of elements to be parsed
+
+    Returns:
+        Boolean: True if a new chapter is found, False otherwise.
+    """
+    global chapter
     global newest
-    chapter = list()
     regex_url = re.compile("http://readms.net/r/one_piece/[0-9]+/[0-9]+/1")
     regex_number_and_name = re.compile("[0-9]+.?-.?[\"a-zA-Z0-9\?\!\,\.\'\:\s]+")
     for element in data:
@@ -36,15 +64,24 @@ def parse(data):
             chapter_url_str = chapter_url.group(0)
             if int(chapter_number) > newest:
                 newest = int(chapter_number)
-                element = chapter_url_str, chapter_number, chapter_name
-                chapter.append(element)
-                return chapter
+                chapter = chapter_url_str, chapter_number, chapter_name
+                return True
             else:
-                return
+                return False
         except:
-            return
+            return False
 
 def parsehtml(html):
+    """
+    Searches bytes for lines with link to One Piece chapter.
+    Returns a list with aforementioned chapters.
+
+    Args:
+        html (bytes): data to be searched for
+
+    Returns:
+       list: a list of data
+    """
     soup = BeautifulSoup(html)
     r = []
     for e in soup.findAll('a', attrs={'href': re.compile("http://readms.net/r/one_piece")}):
@@ -52,41 +89,74 @@ def parsehtml(html):
     return r
 
 def save(savable):
+    """
+    Saves a tuple to a file. Does not return anything.
+
+    Args:
+        savable (tuple): the data that should be saved.
+    """
     output = os.path.join(os.getcwd(),save_location)
     with open(output,'w+') as out:
-        for element in savable:
-            try:
-                line_to_write = element[0] + " " + element[1] + " - " + element[2]
-            except:
-                line_to_write = ''
-            out.write(line_to_write+"\n")
+        try:
+            line_to_write = savable[0] + " " + savable[1] + " - " + savable[2]
+        except:
+            line_to_write = ''
+        out.write(line_to_write+"\n")
 
 def load():
+    """
+    Loads data from a file to a list, in case the file contains
+    more than one line which it shouldn't. Never know though.
+    Returns Boolean True if loading the file was successful,
+    otherwise returns Boolean False and creates the file with
+    nothing in it.
+
+    Returns:
+       Boolean: True if successful, False otherwise.
+    """
+    global chapter
     load_file = os.path.join(os.getcwd(),save_location)
     try:
         with open(load_file,"r") as ins:
             lines = []
             for line in ins:
                 lines.append(line)
-        return parse(lines)
+            if parse(lines):
+                return True
+            else:
+                return False
     except:
         save('')
+        return False
 
-def mainloop():
+def mainloop(n):
+    """
+    The main loop. Runs from here to eternity,
+    sleeping for n seconds between runs.
+    First we check whether or not we can get HTML
+    from the (as of now hard-coded) URL - if we can,
+    we parse this HTML with parsehtml, after which we
+    parse the data provided by parsehtml. If, while parsing,
+    we find that a new chapter has been released, we print
+    a message that tells this and save the info.
+    
+    Args:
+        n (int): time to sleep between runs.
+    """
     while(True):
         data = getfromurl(URL)
         if data:
             loc = parsehtml(data)
             t = parse(loc)
             if t:
-                print("New Chapter: {} - {} {}".format(t[0][1],t[0][2],t[0][0]))
-                save(t)
-        sleep(600)
+                print("New Chapter: {} - {} {}".format(chapter[1],chapter[2],chapter[0]))
+                save(chapter)
+        sleep(n)
 
 if __name__ == "__main__":
     try:
         load()
-        mainloop()
+        mainloop(600)
     except KeyboardInterrupt:
         print("Exiting")
         try:
